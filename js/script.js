dcbaba9aa627851fb0d14b838085f77a7b101cc4
Nekwan Luke <contact@nekwanluke.com>
2019-05-17 19:10:21 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-$(document).ready(function()
-{
-  //code to be executed goes here
-  <script src="js/script.js">
-  </script>
-  
-    <script>
-   $("#text").append("this is so freaking cool.");
-  </script>
-});
-
-
-
 $(document).ready(function() {
   $("#text").append("this is so freaking cool");
 });
